0ec67a57bd0c56ef6250bdd2d9b866abdf5e1e10
Michael Ferraco <ferraco.mike@gmail.com>
2017-02-14 22:32:55 -0500

    Removed static image resources, added MapViews to fragments

--- app/src/main/java/com/mobile/mferraco/weddingblitz/infofragments/CeremonyFragment.java
+++ app/src/main/java/com/mobile/mferraco/weddingblitz/infofragments/CeremonyFragment.java
@@ -7,13 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mobile.mferraco.weddingblitz.R;
 
 /**
  * This fragment will display details about the ceremony.
  */
 
-public class CeremonyFragment extends Fragment {
+public class CeremonyFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mMapView;
 
     public static CeremonyFragment newInstance(Bundle args) {
         CeremonyFragment fragment = new CeremonyFragment();
@@ -25,6 +34,56 @@ public class CeremonyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ceremony, container, false);
+
+        mMapView = (MapView) view.findViewById(R.id.ceremony_google_map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
         return view;
+    }
+
+    /* ========== Google Maps ========== */
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        Marker marker = map.addMarker(new MarkerOptions()
+                .position(new LatLng(39.8858, -79.6484))
+                .title("Marker"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15));
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomIn());
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+    }
+
+    /* ========== LifeCycle Methods ========== */
+
+    /*
+    Need to call all the MapView lifecycle methods from the fragment
+     */
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
