35031fe457294358588aa99896ca558adae05506
Michael Ferraco <ferraco.mike@gmail.com>
2017-02-20 20:02:04 -0500

    Added data loading indicator to the ScheduleFragment

--- app/src/main/java/com/mobile/mferraco/weddingblitz/infofragments/schedule/ScheduleFragment.java
+++ app/src/main/java/com/mobile/mferraco/weddingblitz/infofragments/schedule/ScheduleFragment.java
@@ -2,23 +2,26 @@ package com.mobile.mferraco.weddingblitz.infofragments.schedule;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobile.mferraco.weddingblitz.R;
+import com.mobile.mferraco.weddingblitz.infofragments.DataLoadingFragment;
 import com.mobile.mferraco.weddingblitz.models.Event;
 
 /**
  * This fragment will display the schedule of events.
  */
 
-public class ScheduleFragment extends Fragment {
+public class ScheduleFragment extends DataLoadingFragment {
 
     public static ScheduleFragment newInstance(Bundle args) {
         ScheduleFragment fragment = new ScheduleFragment();
@@ -29,7 +32,11 @@ public class ScheduleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        // inflate content into DataLoadingFragment's content layout
+        getActivity().getLayoutInflater().inflate(R.layout.fragment_schedule, getContentLayout());
+        
         return view;
     }
 
@@ -46,6 +53,19 @@ public class ScheduleFragment extends Fragment {
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("weddings/0/schedule");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                fragmentLoadComplete();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                fragmentLoadComplete();
+            }
+        });
+
         recyclerView.setAdapter(new ScheduleAdapter(Event.class, R.layout.cardview_event, EventViewHolder.class, ref, getContext()));
     }
 }
